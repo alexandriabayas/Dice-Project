85f5270c26dab4a4aebdbd67ef5dd492ae303063
melvinrajendran <44681827+melvinrajendran@users.noreply.github.com>
2018-12-05 10:42:21 -0500

    formatting

--- Die.java
+++ Die.java
@@ -1,14 +1,14 @@
-public class Die{
+public class Die {
 
 	private int sides;
 	private int value;
 
-	public Die(){
+	public Die() {
 		this.sides = 6;
 		this.value = (int)(Math.random()*6)+1;
 	}
 
-	public Die(int sides){
+	public Die(int sides) {
 		this.sides = sides;
 		this.value = (int)(Math.random()*sides)+1;
 	}
