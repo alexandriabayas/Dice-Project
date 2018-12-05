0525dbd7cafd82cd165abb6f7c0f7e60a8f2e463
alexandriabayas <45398974+alexandriabayas@users.noreply.github.com>
2018-12-05 10:11:02 -0500

    Created methods
    Created methods to get values, roll, and toString

--- Dice.java
+++ Dice.java
@@ -13,4 +13,16 @@ public class Dice{
 		value = (int)(Math.random()*sides)+1;
 	}
 
+	public int getValue(){
+		return value;
+	}
+
+	public void roll(){
+		value = (int)(Math.random()*sides)+1;
+	}
+
+	public String toString(){
+		return "Number of sides: " + sides + ", Value: " + value;
+	}
+
 }
