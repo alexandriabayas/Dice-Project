6f7d7c6e5b44339fe0b89951a3e374540eeadd58
alexandriabayas <45398974+alexandriabayas@users.noreply.github.com>
2018-12-07 11:44:08 -0500

    Update DiceRunner.java

--- DiceRunner.java
+++ DiceRunner.java
@@ -1,10 +1,15 @@
 public class DiceRunner{
 	public static void main (String[]args){
 
+		//Test 1
 		Die die1 = new Die();
 		Die die2 = new Die();
 
-		int rolls = 0;
+		int rolls = 1;
+
+		System.out.println("Die 1: " + die1.toString());
+		System.out.println("Die 2: " + die2.toString());
+		System.out.println();
 
 		while(die1.getValue() != 1 || die2.getValue() != 1){
 			die1.roll();
@@ -18,12 +23,14 @@ public class DiceRunner{
 		System.out.println("It took " + rolls + " rolls to get snake eyes");
 		System.out.println();
 
+		//Test 2
+
 		DiceHolder hold = new DiceHolder();
 
 		int i = 1;
 		while(i != -1){
 			System.out.println(hold.toString());
-			i = hold.addDie(new Die((int)(Math.random()*20)+1));
+			i = hold.addDie(new Die((int)(Math.random()*17)+4));
 		}
 
 		System.out.println();
